b12f97536943002e6903e4be4ed0351ed98f3533
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-04-20 15:51:00 +0200

    Creating our data model

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,14 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'speaker',
+        loadChildren: () => import('./conference/speaker/speaker.module').then(m => m.ConferenceSpeakerModule)
+      },
+      {
+        path: 'session',
+        loadChildren: () => import('./conference/session/session.module').then(m => m.ConferenceSessionModule)
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ]
